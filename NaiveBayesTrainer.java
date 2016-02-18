d334d62a725f8cce3b8be6945cdc4b6bc2ce0d3c
Brian Yan <score14@gmail.com>
2016-02-17 16:20:32 -0800

    start to classify

--- NaiveBayesTrainer.java
+++ NaiveBayesTrainer.java
@@ -4,8 +4,11 @@ import java.io.*;
 public class NaiveBayesTrainer{
 	public static double posR = 0;
 	public static double negR = 0;
+	public static int TotalWordsInCatPos = 0;
+	public static int TotalWordsInCatNeg = 0;
 	public HashMap<String, ArrayList<Double> > map = new HashMap<String, ArrayList<Double> >();
-	
+	// Array List is 0 index -> positive word count , 1 index -> negative word count
+	// 2 index -> 
 	public NaiveBayesTrainer(){
 
 	}
@@ -20,6 +23,10 @@ public class NaiveBayesTrainer{
         	Review review = new Review(line.split("\\s"));
         	for(int i=1; i< review.words.length; i++){
         		// if word not in hashmap
+        		if(review.cat == Category.POS)
+        			incrementWordsInCatPos();
+        		else
+        			incrementWordsInCatNeg();
         		if(!map.containsKey(review.words[i])){
         			if(review.cat == Category.POS){
         				ArrayList<Double> temp = new ArrayList<Double>();
@@ -66,6 +73,23 @@ public class NaiveBayesTrainer{
         }
         br.close();
         //System.out.println(getposR());
+
+        for(String s: map.keySet()){
+        	ArrayList<Double> temp = map.get(s);
+        	temp.set(2,this.WordGivenCategory(s,Category.POS));
+        	temp.set(3,this.WordGivenCategory(s,Category.NEG));
+        	// //get probrability of word (word given cat neg)
+        	// // and word given cat pos
+        	// int positivewords = temp.get(0);
+        	// int negativewords = temp.get(1);
+        	// int vocab = getVocabulary();
+        	// int totalnumberwordsPos = this.getTotalWordsinCatPos();
+        	// int totalnumberwordsNeg = this.getTotalWordsinCatNeg();
+        	// double prob 
+        	// temp.set(2)
+        }
+
+
         for(String s: map.keySet()){
         		System.out.print(s + " count: ") ;
         		for(Double d: map.get(s))
@@ -75,14 +99,70 @@ public class NaiveBayesTrainer{
 	System.out.println(getposR());
 	System.out.println(getnegR());
 	System.out.println(this.probability_CategoryPos());
+	System.out.println(this.TotalWordsInCatPos);
+	System.out.println(this.TotalWordsInCatNeg);
+	System.out.println(getVocabulary());
+
+	}
+	public void classify(String filename) throws Exception{
+		String line = null;
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+        while((line = br.readLine()) != null){
+
+
+
+
+        }
 	}
 	public String normalize(String s) {
-		String [] Stopwords = {"?", "." , ",", "/", "!" };
+		String [] Stopwords = {"?", "." , ",", "/", "!" , "("  ,")"
+		, "'", "-", "\"", ">", "<"};
 		for(String c: Stopwords){
-			s = s.replace(c, "");
+			s = s.replace(c, " ");
 		}	
 		return s.toLowerCase();
 	}
+
+	public double WordGivenCategory(String word, Category cat) {
+		int c = (cat == Category.POS) ? 1 : 0;
+		int totalwords = 0;
+		double numOfThisCat = 0;
+		if(c == 1) 
+			totalwords = getTotalWordsinCatPos();
+		else
+			totalwords = getTotalWordsinCatNeg();
+		//if the word is in the map, use that data
+		if(map.containsKey(word)){
+			if(c == 1)
+				numOfThisCat = map.get(word).get(0); //get count of occurences that this word appears in pos category
+			else
+				numOfThisCat = map.get(word).get(1); //get count of occurences that this word appears in neg category
+		}
+		//otherwise use 0
+		else{
+			numOfThisCat = 0;
+		}
+		double smooth = 1;
+		return ((numOfThisCat+smooth)/(totalwords + getVocabulary() ));
+	}
+	/* returns the total number of words in the set */
+	public int getVocabulary(){
+		return this.map.size();
+	}
+
+	// public double calculate(String keyWord, String category) {
+	// 	if (probCache.contains(keyWord, category)) {
+	// 		return probCache.get(keyWord, category);
+	// 	}
+	// 	Integer Nxc = tdm.numberOfWord(keyWord, category);
+	// 	Integer Nc = tdm.totalNumberOfWords(category);
+	// 	Integer V = tdm.getVocabulary();
+
+	// 	Double prob = (Nxc + 1) / (Nc + M + V);
+	// 	probCache.add(keyWord, category, prob);
+
+	// 	return prob;
+	// }
 	public double probability_CategoryPos() {
 		//prob is just #occurances/#total
 			return (getposR() / totalR());
@@ -92,15 +172,23 @@ public class NaiveBayesTrainer{
 		return getnegR() / totalR();
 	}
 
-	// public double proabilitywordgivencategory(String word, Category cat){
-
-	// }
-
 	public void incrementpos(){
 		this.posR++;
 	}
 	public void incrementneg(){
 		this.negR++;
+	}
+	public void incrementWordsInCatPos(){
+		this.TotalWordsInCatPos++;
+	}
+	public void incrementWordsInCatNeg(){
+		this.TotalWordsInCatNeg++;
+	}
+	public int getTotalWordsinCatPos(){
+		return this.TotalWordsInCatPos;
+	}
+	public int getTotalWordsinCatNeg(){
+		return this.TotalWordsInCatNeg;
 	}
 	public double getnegR() {
         return this.negR;
